f621b0237e1a174fc5d3eee6885d16a5b242b350
Mahdialipoo <m.manzelsaz@gmail.com>
2022-06-06 16:06:38 +0430

    PID is OK

--- controllers/rcj_soccer_team_blue/PID.py
+++ controllers/rcj_soccer_team_blue/PID.py
@@ -1,15 +1,20 @@
 
 class PID():
-    I=0
-    y=[0,0]
-    def __init__(self,kd,kp,ki,T) :
+    def __init__(self,kd,kp,ki,T,N=100.0) :
         self.kd=kd
         self.kp=kp
         self.ki=ki
         self.T=T
+        self.I=0
+        self.y=[0,0]
+        self.D=0.0
+        self.N=N
     def run(self,x):
+
         self.y[0]=self.y[1]
-        self.y[1]=self.kp*x[1]+self.I+(self.kd/self.T)*(x[1]-x[0])
+        self.y[1]=self.kp*x[1]+self.I+self.kd*self.D
         self.I=self.ki*self.T*x[1]+self.I
+        self.D=(self.D+x[1]-x[0])/(self.N*self.T+1)
+        x[0]=x[1]
         return self.y
         
--- controllers/rcj_soccer_team_blue/robot1.py
+++ controllers/rcj_soccer_team_blue/robot1.py
@@ -1,6 +1,7 @@
 # rcj_soccer_player controller - ROBOT B1
 
 # Feel free to import built-in libraries
+from cmath import atan
 import math
 from os import times_result  # noqa: F401
 
@@ -9,21 +10,24 @@ import utils
 from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
 from time import time,sleep
 from PID import PID
-from math import sqrt
+from math import sqrt,atan2
 class MyRobot1(RCJSoccerRobot):
     def run(self):
-        T=0.0005
+        #****************************mycode
+        T=0.0010
         L=0.085
         R=0.02
-        xd=-0.4
-        yd=-0.4
-        phd=0
-        w=0 
+        xd=-0.45
+        yd=0.0
+        vl=0.0
+        vr=0.0
         e_r=[0,0] 
+        e_p=[0,0]
         robot_pos = self.get_gps_coordinates()
         robot_com=self.get_compass_heading()
-        C_r=PID(kd=0,kp=0.2,ki=0,T=T)     
-        C_ph=PID(kd=0,kp=0.2,ki=0,T=T)
+        C_r=PID(kd=0.05,kp=0.15,ki=0.01,T=T)     
+        C_p=PID(kd=0.0,kp=0.05,ki=0.1,T=T)
+        #****************************my code
         while self.robot.step(TIME_STEP) != -1:
             start=time()
             if self.is_new_data():
@@ -52,28 +56,35 @@ class MyRobot1(RCJSoccerRobot):
 
                 self.left_motor.setVelocity(10) # self code
                 #******************************************************************mycode
-                rd=sqrt(xd**2+yd**2)
+                #er=self.get_new_ball_data()
                 robot_pos = self.get_gps_coordinates()
                 robot_com=self.get_compass_heading()
-                r=sqrt(robot_pos[0]**2+robot_pos[1]**2)
-                e_r[0]=e_r[1]
-                e_r[1]=rd-r
+                xd=ball_data['direction'][1]
+                yd=ball_data['direction'][0]
+                e_r[1]=sqrt((robot_pos[0]-yd)**2+(robot_pos[1]-xd)**2)
+                e_p[1]=robot_com-atan2(yd-robot_pos[0],xd-robot_pos[1])
                 u=C_r.run(e_r)
-                #eph=phd-robot_com
-                w=0
-                vl=(2*u[1]+L*w)/(2*R)
-                vr=(2*u[1]-L*w)/(2*R)
-                if(vl<2):
-                    vl=0
-                if(vr<2):
-                    vr=0
+                w=C_p.run(e_p)
+                vr_=(2*u[1]+L*w[1])/(2*R)
+                vl_=(2*u[1]-L*w[1])/(2*R)
+                print(ball_data)
+                #print(f"xd-x :{robot_pos[1]-xd} ,yd-y :{robot_pos[0]-yd}")
+                #****************dead zone:
+                if(abs(vl_)>0.5):
+                    vl=vl_
+                else:
+                    vl=vl*0.8
+                if(abs(vr_)>0.5):
+                    vr=vr_
+                else:
+                    vr=vr*0.8
                 self.left_motor.setVelocity(vl)
                 self.right_motor.setVelocity(vr)
                 end=time()
                 sleep(T-(end-start))
-                #*************************************************************************
-                '''# Compute the speed for motors
-                direction = utils.get_direction(ball_data["direction"])
+                #******************************************************************my code
+                # Compute the speed for motors
+                '''direction = utils.get_direction(ball_data["direction"])
 
                 # If the robot has the ball right in front of it, go forward,
                 # rotate otherwise
